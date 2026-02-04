ddac3dec366a6c0471c05df761e617fdc9996298
Traian Alexandru <victor_traian@yahoo.com>
2026-02-03 21:14:05 -0500

    edit

--- src/app/wiki/edit/[id]/page.tsx
+++ src/app/wiki/edit/[id]/page.tsx
@@ -1,5 +1,8 @@
+import { notFound } from "next/navigation";
 import WikiEditor from "@/components/wiki-editor";
+import { getArticleById } from "@/lib/data/articles";
 import { stackServerApp } from "@/stack/server";
+
 
 interface EditArticlePageProps {
   params: Promise<{
@@ -10,9 +13,34 @@ interface EditArticlePageProps {
 export default async function EditArticlePage({
   params,
 }: EditArticlePageProps) {
-  await stackServerApp.getUser({ or: "redirect" });
   const { id } = await params;
+  const _user = await stackServerApp.getUser({ or: 'redirect' });
 
+  // we'll uncomment this later when the articles have real IDs
+  // if (user.id !== id) {
+  //   stackServerApp.redirectToHome();
+  // }
+
+  // In a real app, you would fetch the article data here
+  // For now, we'll just show some mock data if it's not "new"
+  if (id === 'new') {
+    return <WikiEditor isEditing={true} articleId={id} />;
+  }
+
+  const article = await getArticleById(+id);
+  if (!article) {
+    notFound();
+  }
+  return (
+    <WikiEditor
+      initialTitle={article.title}
+      initialContent={article.content}
+      isEditing={true}
+      articleId={id}
+    />
+  );
+}
+/*
   // In a real app, you would fetch the article data here
   // For now, we'll just show some mock data if it's not "new"
   const mockData =
@@ -46,3 +74,4 @@ This would normally be fetched from your API.`,
     />
   );
 }
+*/
